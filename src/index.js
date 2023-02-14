c5265a7a0f6ab98eb445b4c35f94202f27e32974
kraskakraskova <118130412+kraskakraskova@users.noreply.github.com>
2023-02-14 18:11:27 +0400

    init: code the solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    n = Math.abs(n);
+    let str = String(n);
+    let res = "";
+
+    for (i = str.length - 1; i >= 0; i--) {
+        if (n[n.length - 1] !== 0) {
+            res = res + str[i];
+        }
+    }
+    return res;
+};
